4452226cca7445b5da9ae73befe7c6fc26b0f483
Denis Morozov <denis.moroz.morozov@gmail.com>
2020-08-23 20:49:57 +0300

    Fix inits

--- Sources/ChatBotSDK/ReplyKeyboardMarkup.swift
+++ Sources/ChatBotSDK/ReplyKeyboardMarkup.swift
@@ -4,8 +4,22 @@ public struct ReplyKeyboardMarkup {
     public var keyboard: [[KeyboardButton]]
     public var resizeKeyboard: Bool
     public var oneTimeKeyboard: Bool
+
+    public init(
+        keyboard: [[KeyboardButton]],
+        resizeKeyboard: Bool,
+        oneTimeKeyboard: Bool
+    ) {
+        self.keyboard = keyboard
+        self.resizeKeyboard = resizeKeyboard
+        self.oneTimeKeyboard = oneTimeKeyboard
+    }
 }
 
 public struct KeyboardButton {
     public var text: String
+
+    public init(text: String) {
+        self.text = text
+    }
 }
